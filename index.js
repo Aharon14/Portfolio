53940e6c369090af7fbe67fb8402e8fe1d94c73b
Aharon Tadi <127577180+Aharon14@users.noreply.github.com>
2025-08-05 13:19:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ window.addEventListener("load", () => {
   const loader = document.getElementById("loader");
   setTimeout(() => {
     loader.classList.add("fade-out");
-  }, 500); // delay before fade-out
+  }, 1500); // delay before fade-out
 });
 
 
